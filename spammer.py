1794dfd56ec88005a84fd20ad590e29453097016
idosharon <michdodim@gmail.com>
2019-04-13 18:22:49 +0300

    Cool update

--- spammer.py
+++ spammer.py
@@ -10,20 +10,21 @@ from pynput.keyboard import Key, Controller
 from distutils.util import strtobool
 
 
-#### All the functions:
+#### All the Modes functions:
 
 # image spammer function
 def images_spammer():
+	print("Ok, so images spaming! let's get started!\n")
+
+	image_subject = "random"
+
 
 	# image random ask
-	if strtobool(raw_input("	Would You like a random images? [Yes/No] : ")):
-		print "	Okay, I will spam", target ,"with random images"
+	if strtobool(raw_input("\n\n	Would You like a random images? [Yes/No] : ")):
+		print "	Okay, I will spam", target ,"with",image_subject,"images"
 	else:
 		image_subject = raw_input("	Okay, what is the subject of your images?: ").lower()
 		image_subject = "1600x900/?" + image_subject
-
-	# some text
-	print("\nimage_subject is: " + image_subject)
 
 	for i in range(min, max + 1):
 		# open unsplash
@@ -32,6 +33,7 @@ def images_spammer():
 		if driver.current_url == "https://images.unsplash.com/source-404?fit=crop&fm=jpg&h=800&q=60&w=1200":
 			print("Please enter a real subject")
 			exit()
+			driver.close()
 
 		# copy img
 		keyboard.press(Key.cmd)
@@ -47,7 +49,6 @@ def images_spammer():
 
 		# find target
 		x_arg = '//span[contains(@title,"' + target + '")]'
-		print("x_arg is :", x_arg)
 		group_title = wait.until(EC.presence_of_element_located((
 			By.XPATH, x_arg)))
 		group_title.click()
@@ -66,38 +67,68 @@ def images_spammer():
 		keyboard.release(Key.enter)
 
 		# delay
-		time.sleep(5)
+		time.sleep(4.5)
 
+		if i == max+1:
+			print("Thank you! I finish here.")
+
+# text spammer function
+def text_spammer(max):
+	max = max
+	print("Alright, so text spaming! let's get started!")
+	massage = raw_input("\n\n	What is your massage?: ")
+	max = input("\n	because it's text spamming you may want to send more, so you can change the num of massages now: ")
+	print("max is: " + str(max))
+
+	# open whatsapp
+	driver.get("https://web.whatsapp.com/")
+
+	# delay
+	time.sleep(4)
+
+	# find target
+	x_arg = '//span[contains(@title,"' + target + '")]'
+	group_title = wait.until(EC.presence_of_element_located((
+		By.XPATH, x_arg)))
+	group_title.click()
+
+	for i in range(min, max + 1):
 		# enter i into massage
 		message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
-		message.send_keys("this is image number ",str(i)," from ",str(max)," images")
+		message.send_keys(massage,i)
 
-		# send i
+		# send massage
 		sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
 		sendbutton.click()
 
-		# delay
-		time.sleep(4.5)
+		if i == max+1:
+			print("Thank you! I finish here.")
+
+# sticker spammer function
+def sticker_spammer():
+	print("So sticker spaming! let's get started!\n")
 
 
-print("Welcome to the whatsapp web image spammer! \nplease answer the next questions:\n")
+print("\n\nWelcome to the whatsapp web image spammer! \nplease answer the next questions:\n")
 
 # vars
 image_subject = "random"
 min = 0
-max = input("	please enter how much would you like to spam your frind?: ")
+max = input("	please enter how much would you like to spam your frind? \n	** don't make a large number it will take a lot of time and it can make bugs!\n 	so please:")
 if max <= 0:
 	print("			You have to enter a number that is bigger then 0, try again later")
 	exit()
+	driver.close()
 
 # target
 target = raw_input("	Who is the target?: ")
 if len(target) < 0:
-	print("			You have to enter a target, try again later")
+	print("			! You have to enter a target, try again later")
 	exit()
+	driver.close()
 
 # mode
-mode = raw_input("Please choose mode:\n		(a)image spaming[type a]\n		(b) text spaming[type b]\n		(c) sticker spaming[type c]\n")
+mode = raw_input("	Please choose mode:\n		(a) image spaming [type a]\n		(b) text spaming [type b]\n		I choose: ")
 
 # drivers
 keyboard = Controller()
@@ -110,10 +141,8 @@ wait = WebDriverWait(driver, 600)
 if mode == "a":
 	images_spammer()
 elif mode == "b":
-	text_spammer()
-elif mode == "c":
-	sticker_spammer()
+	text_spammer(max)
 else :
-	print("please next time enter a real mode")
-	driver.close()
+	print("	! please next time enter a real mode")
 	exit()
+	driver.close()
